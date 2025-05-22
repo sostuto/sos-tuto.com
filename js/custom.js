53bc5f026d7be5d0f1ec378adda5f2a4c4f27eaf
sostuto <sostuto228@gmail.com>
2025-05-22 08:45:33 +0000

    second update

--- js/custom.js
+++ js/custom.js
@@ -70,7 +70,7 @@ jQuery(document).ready(function($) {
 		});
     $('#post-slider').flexslider({
         // Primary Controls
-        controlNav          : false,              //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
+        controlNav          : true,              //Boolean: Create navigation for paging control of each clide? Note: Leave true for manualControls usage
         directionNav        : true,              //Boolean: Create navigation for previous/next navigation? (true/false)
         prevText            : "Previous",        //String: Set the text for the "previous" directionNav item
         nextText            : "Next",            //String: Set the text for the "next" directionNav item
